9c18a2784d70ef2325e64b028235030a7ae76df0
AuZzoo <austenzhu@berkeley.edu>
2017-01-18 11:42:20 -0800

    Changes in order to compile dependancies - First Commit
    Taken from
    http://stackoverflow.com/questions/36397171/aws-lambda-not-importing-lxml

---
+++ worker.py
@@ -0,0 +1,10 @@
+import lxml
+import requests
+import cssselect
+
+def sample_function( input_string = None):
+    return "Imports Successful!"
+
+if __name__ == "__main__":
+    result = sample_function()
+    print result 
